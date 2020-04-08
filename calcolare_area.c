887404b6617ded5b6a4176e55889ae8391ebb4df
Besnik Duro <nikduro@gmail.com>
2020-04-08 17:46:18 +0200

    inserito calcolare_area.c

---
+++ calcolare_area.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc,char* argv[])
+{
+  int base, altezza, area;
+  base = 5;
+  altezza = 1;
+  area = base * altezza;
+  printf("area è = %d\n", area);
+  return 0;
+}
